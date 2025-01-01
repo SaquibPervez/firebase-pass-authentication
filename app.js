83ee5bb42b3ec0a1e4d3c030f9d34cb1f49e8673
Saquib <saquibpervez01@gmail.com>
2025-01-01 17:59:09 +0500

    done

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-import {auth, getAuth, createUserWithEmailAndPassword }
-from "./firebase.js"
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from "./firebase.js";
 
+const signup = async () => {
+  try {
+    const email = document.getElementById('emailInput1').value;
+    const password = document.getElementById('passwordInput1').value;
 
-const signup = async () =>{
-    try{
-        const email = "admin@example.com"
-        const password = "abc123"
-        const user = await createUserWithEmailAndPassword(auth, email, password)
-        console.log(user)
-    }catch(error){
-        console.log(error.message)
-    }
-}
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    alert("Signup Success: " + userCredential.user);
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+  }
+};
 
-window.signup = signup
+const login = async () => {
+  try {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    console.log("User logged in:", user.user.email);
+    alert("Account Successfully Logged In");
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+  }
+};
+
+window.signup = signup;
+window.login = login;
